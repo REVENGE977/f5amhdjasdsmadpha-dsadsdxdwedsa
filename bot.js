9e8131c11a99d094b632eff4c0f3cac61315f973
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-09-23 13:53:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -804,5 +804,7 @@ client.on('guildMemberAdd', member => {
      stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
    //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
-
+if(message.content.startsWith('السلام عليكم')) {
+return message.reply('وعليكم السلام ورحمه الله وبركاته')
+}
 client.login(process.env.BOT_TOKEN)
